660e3f9aed95a3158d4a8e30b34c91bfe5a6edcb
ginniImStupid <jigmeepaotamang@gmail.com>
2026-03-11 09:07:45 +0530

    Coded UC1: Display Hello World in console

---
+++ src/main/java/HelloApp.java
@@ -0,0 +1,9 @@
+/* HelloApp.java - Simple Application that prints "Hello World" into console
+ * @author jigmeepaotamang
+ * @version 1.0
+ * */
+public class HelloApp {
+  public static void main(String args[]) {
+    System.out.println("Hello World!");
+  }
+}
