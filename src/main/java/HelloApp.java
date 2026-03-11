db00def68cbbe8230c763a7a532d1f74be196bb1
ginniImStupid <jigmeepaotamang@gmail.com>
2026-03-11 09:14:32 +0530

    Coded UC2: Display Hello with Command-Line Argument

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,9 +1,11 @@
 /* HelloApp.java - Simple Application that prints "Hello World" into console
  * @author jigmeepaotamang
  * @version 1.0
+ * @since UC1
  * */
 public class HelloApp {
   public static void main(String args[]) {
-    System.out.println("Hello World!");
+    String name = args[0];
+    System.out.println("Hello, " + name + "!");
   }
 }
