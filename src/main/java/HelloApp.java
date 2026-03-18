86c91f48bd31e8ea64c5f60763b69dcccbf7d79a
ginniImStupid <jigmeepaotamang@gmail.com>
2026-03-18 08:20:23 +0530

    Coded UC3

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,11 +1,11 @@
 /* HelloApp.java - Simple Application that prints "Hello World" into console
  * @author jigmeepaotamang
- * @version 1.0
- * @since UC1
+ * @version 3.0
+ * @since UC3
  * */
 public class HelloApp {
   public static void main(String args[]) {
-    String name = args[0];
+    String name = "World";
     System.out.println("Hello, " + name + "!");
   }
 }
