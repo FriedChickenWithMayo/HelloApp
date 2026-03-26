996d747deaeff77e847e4923230e8784b22d4c53
Jigmee Pao Tamang <jigmeepaotamang@gmail.com>
2026-03-26 09:37:29 +0530

    Create HelloApp.java

---
+++ HelloApp.java
@@ -0,0 +1,10 @@
+/* HelloApp.java - Simple Application that prints "Hello World" into console
+ * @author jigmeepaotamang
+ * @version 1.0
+ * @since UC1
+ * */
+public class HelloApp {
+  public static void main(String args[]) {
+    System.out.println("Hello, World!");
+  }
+}
