a5b74c6af2e8faa12336c636d65d373f610fe779
FriedChickenWithMayo <jigmeepaotamang@gmail.com>
2026-03-26 09:29:20 +0530

    Simplified the program

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,22 +1,13 @@
 /* HelloApp.java - Simple Application that prints "Hello World" into console
  * @author jigmeepaotamang
- * @version 5.0
+ * @version 6.0
  * @since UC1
  * */
 public class HelloApp {
     public static void main(String[] args) {
-        StringBuilder nameBuilder = new StringBuilder();
-        String finalNames;
-        boolean first = true; 
-        for (String s : args) { 
-          if (!first) {
-                nameBuilder.append(", ");
-            }
-            nameBuilder.append(s);
-            first = false;
-        }
-
-        finalNames = nameBuilder.toString();
-        System.out.println("Hello, " + finalNames + "!");
+        String names = "World"; 
+        if(args.length>0)
+          names=String.join(", ", args);
+        System.out.println("Hello, " + names + "!");
     }
 }
