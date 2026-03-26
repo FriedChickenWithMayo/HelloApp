bc97f7b8ec86298f71275988bfe1138d98e129af
FriedChickenWithMayo <jigmeepaotamang@gmail.com>
2026-03-26 09:07:01 +0530

    Adjusted for loop for cleaner iteration without index management

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,18 +1,22 @@
 /* HelloApp.java - Simple Application that prints "Hello World" into console
  * @author jigmeepaotamang
- * @version 4.0
+ * @version 5.0
  * @since UC1
  * */
 public class HelloApp {
-  public static void main(String args[]) {
-    StringBuilder nameBuilder = new StringBuilder();
-    String name;
-    for (int i = 0; i < args.length; i++) {
-      nameBuilder.append(args[i]);
-      if (i < args.length - 1)
-        nameBuilder.append(", ");
+    public static void main(String[] args) {
+        StringBuilder nameBuilder = new StringBuilder();
+        String finalNames;
+        boolean first = true; 
+        for (String s : args) { 
+          if (!first) {
+                nameBuilder.append(", ");
+            }
+            nameBuilder.append(s);
+            first = false;
+        }
+
+        finalNames = nameBuilder.toString();
+        System.out.println("Hello, " + finalNames + "!");
     }
-    name = nameBuilder.toString();
-    System.out.println("Hello, " + name + "!");
-  }
 }
